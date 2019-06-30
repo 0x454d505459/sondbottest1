12773dd77c160f7d5e6d3e06bbc8e85db3b00958
TheFourchette <40660493+TheFourchette@users.noreply.github.com>
2019-06-30 11:08:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', function(member){
-    member.addRole('559395818139025408').then(.catch(console.error))
+    member.addRole('559395818139025408').then(catch(console.error))
     member.createDM().then(function (channel){
         return channel.send('Bienvenue ' + member.displayName + ' sur notre serveur nous te shouhaitons une bonne journée/soirée et merci de lire le règlement.:) ')
     }).catch(console.error)
