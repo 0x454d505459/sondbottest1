81f4ddbf03d300839111d3c4f677fcb0af9a4697
TheFourchette <40660493+TheFourchette@users.noreply.github.com>
2020-03-28 15:56:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 var client = new Discord.Client()
-client.login(env.process.TOKEN)
+client.login(process.env.TOKEN)
 var prefix = ("/");
 var adminprfix= ("//")
 client.on('ready', () => {
