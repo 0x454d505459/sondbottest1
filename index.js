139eef14796637702eeff2fd392bf25e0eb38bde
TheFourchette <40660493+TheFourchette@users.noreply.github.com>
2020-03-28 17:18:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ client.on('message', message => {
     }
 	
 	if(message.content === prefix + "animate") {
-        	if(!message.member.hasPermission("MANAGE_SERVER")) {return message.channel.send("Permission manquante: MANAGE_SERVER")} {
+        	if(!message.member.hasPermission("MANAGE_GUILD")) {return message.channel.send("Permission manquante: MANAGE_SERVER")} {
             
             inet = setInterval(anim, 80)
         }
